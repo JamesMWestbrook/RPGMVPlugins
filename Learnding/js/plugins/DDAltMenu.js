b8612e295ddec4b53e3abf239e2240f290ba8cd1
d7c2180203df31ff80a6fc3cdca19be153e5d881 <james.michael.westbrook@gmail.com>
2019-10-03 18:06:36 -0500

    Start of project

---
+++ Learnding/js/plugins/DDAltMenu.js
@@ -0,0 +1,20 @@
+
+//=============================================================================
+// DDAltMenu.js
+// By <Author>
+// 
+// Version: <version>
+// Released under <take me daddy>//=============================================================================
+
+/*:
+	@plugindesc Edited title menu for practice in plugins and general UI
+	@author DecayingDev
+
+	@help This plugin does not provide plugin commands.
+
+*/
+
+(function(){
+	
+
+})
